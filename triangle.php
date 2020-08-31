58a84113a7e32c1e34b66bd518c37e1e2e0e67c2
vladimirov.ubuntu <vladimirov.p@utsmow.ru>
2020-08-31 12:54:53 +0700

    isValidTriangle - проверка, можно ли составить  треугольник с помощью разностей

--- triangle.php
+++ triangle.php
@@ -1,13 +1,16 @@
 <?php
 
 /**
- * Triangle check
+ * Для значений около PHP_INT_MAX нельзя проверять суммой,
+ * Проверим по теореме: Каждая сторона треугольника больше разности двух других сторон.
+ * Кстати, она вытекает из первой теоремы.
  * @param int $a
  * @param int $b
  * @param int $c
  * @return bool
  */
 function isValidTriangle(int $a, int $b, int $c): bool {
-    return ($a < ($b + $c)) && ($b < ($a + $c)) && ($c < ($a + $b));
+    //return ($a < ($b + $c)) && ($b < ($a + $c)) && ($c < ($a + $b));
+    return ($a > abs($b-$c) && $b > abs($a-$c) && $c > abs($a-$b));
 }
 
